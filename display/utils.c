c7860913fd76c5b6e8c59529cab93b75b7128036
Ismail Mittous <imittous@e2r2p15.1337.ma>
2023-01-23 00:58:41 +0100

    Mon Jan 23 00:58:41 +01 2023

--- display/utils.c
+++ display/utils.c
@@ -73,3 +73,101 @@ void ft_draw_line(t_data    *sd , int len)
         i++;
    }
 }
+// #include "cub3d.h"
+
+// int	create_rgb(int r, int g, int b)
+// {
+// 	return (r << 16 | g << 8 | b);
+// }
+
+// void	ft_draw_ceiling(t_data *data, double start, double len)
+// {
+// 	int	color;
+
+// 	color = create_rgb(data->color.f[2], data->color.f[1], data->color.f[0]);
+// 	while (start <= len)
+// 	{
+// 		my_mlx_pixel_put(&data->img, data->id, start, color);
+// 		start++;
+// 	}
+// }
+
+// void	ft_draw_floor(t_data *data, double start, double len)
+// {
+// 	int	color;
+
+// 	color = create_rgb(data->color.c[2], data->color.c[1], data->color.c[0]);
+// 	while (start <= len)
+// 	{
+// 		my_mlx_pixel_put(&data->img, data->id, start, color);
+// 		start++;
+// 	}
+// }
+
+// void	ft_drawing_wall(int start, int len, t_data *data)
+// {
+// 	int	color;
+
+// 	color = create_rgb(45, 97, 34);
+// 	// ft_draw_ceiling(data, 0, start);
+// 	// ft_draw_floor(data, start + len, W_HEIGHT);
+// 	if (len > W_HEIGHT)
+// 		len = W_HEIGHT;
+// 	while (start < len)
+// 	{
+// 		my_mlx_pixel_put(&data->img, data->id, start, color);
+// 		start++;
+// 	}
+// 	printf ("start: %d\n, len: %d\n", start, len);
+// }
+
+
+// int	ft_rendering_wall(t_data *data)
+// {
+// 	double		wall_height;
+// 	double		wall_start;
+// 	double		dis_projplane;
+// 	double		rayangle;
+
+// 	rayangle = FOFV / W_WIDTH;
+// 	dis_projplane = (W_WIDTH / 2) / tan(FOFV / 2);
+// 	data->id = 0;
+// 	while (data->id < W_WIDTH)
+// 	{
+// 		wall_height = (TILE / data->dis_rays[data->id]) * dis_projplane;
+// 		wall_start = W_HEIGHT / 2 - (wall_height / 2);
+// 		my_mlx_pixel_put(&data->img, data->id, W_HEIGHT / 2 - (wall_height / 2), \
+// 			create_rgb(255, 255, 255));
+// 		ft_drawing_wall(wall_start, wall_height, data);
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +1), \
+// 		// 	create_rgb(255, 255, 255));
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +2), \
+// 		// 	create_rgb(255, 255, 255));
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +3), \
+// 		// 	create_rgb(255, 255, 255));
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +4), \
+// 		// 	create_rgb(255, 255, 255));
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +5), \
+// 		// 	create_rgb(255, 255, 255));
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +6), \
+// 		// 	create_rgb(255, 255, 255));
+
+
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +wall_height-6), \
+// 		// 	create_rgb(255, 255, 255));
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +wall_height-7), \
+// 		// 	create_rgb(255, 255, 255));
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +wall_height-3), \
+// 		// 	create_rgb(255, 255, 255));
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +wall_height-4), \
+// 		// 	create_rgb(255, 255, 255));
+// 		// my_mlx_pixel_put(&data->img, data->id, (W_HEIGHT / 2 - (wall_height / 2) +wall_height-5), \
+// 		// 	create_rgb(255, 255, 255));
+
+// 		data->id++;
+// 	}
+// 	return (data->id = 0, 0);
+// }
+
+
+		
