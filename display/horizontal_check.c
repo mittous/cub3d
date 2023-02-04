ae7835398f7ea4d9ccee81ecb02ae4d328c49e0b
Mohammed badr-eddine El housni <mel-hous@e2r3p11.1337.ma>
2023-02-04 11:17:50 +0100

    Sat Feb  4 11:17:50 +01 2023

--- display/horizontal_check.c
+++ display/horizontal_check.c
@@ -6,63 +6,64 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 11:34:29 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/01/25 11:20:36 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/02/04 11:16:32 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-void ft_horizontal_check(double ang, t_data *sd, double *x, double *y)
+void ft_horizontal_check(double ang, t_data *sd)
 {
     if ((ang >  ((3 *M_PI)/2) && ang < (2 * M_PI)) ||(ang > 0 &&  ang < (M_PI/2)))
     {
-        if (ang >  ((3 *M_PI)/2) && ang < (2 * M_PI))
+        // puts("1\n");
+        if (sd->line->ray_up == 1 && sd->line->ray_right == 1)
         {
-            *y = abs(sd->p->y / 30) * 30;
-            *x = sd->p->x + ((sd->p->y - *y) / tan(ang));
-            while ((int)(*y/TILE_SIZE) > 0 &&sd->cube->map[(int)(*y / TILE_SIZE)][(int)(*x / TILE_SIZE)] != '1')
+            sd->line->h_y = abs(sd->p->y / 30) * 30;
+            sd->line->h_x = sd->p->x + ((sd->line->h_y - sd->p->y) / tan(ang));
+            while ((int)(sd->line->h_y/TILE_SIZE) > 0 &&sd->cube->map[(int)(sd->line->h_y / TILE_SIZE)][(int)(sd->line->h_x / TILE_SIZE)] != '1')
             {
-                *y -= TILE_SIZE;
-                *x += (TILE_SIZE / tan(ang));
+                sd->line->h_y -= TILE_SIZE;
+                sd->line->h_x += (TILE_SIZE / tan(ang));
             }
-            // printf("#####X=%lf--#########Y=%lf\n",*x/ TILE_SIZE,*y/ TILE_SIZE);
+            // printf("#####X=%lf--#########Y=%lf\n",sd->line->h_x/ TILE_SIZE,sd->line->h_y/ TILE_SIZE);
         }
-        if (ang > 0 &&  ang < (M_PI/2))
+        if (sd->line->ray_down == 1 && sd->line->ray_right == 1)
         {
-            *y = (abs(sd->p->y / 30) + 1) * 30;
-            *x = sd->p->x + ((*y - sd->p->y) / tan(ang));
-            while (((int)(*y/TILE_SIZE) > 0 && (int)(*y/TILE_SIZE) < WIN_HEIGHT/TILE_SIZE) && ((int)(*x/TILE_SIZE) > 0 && (int)(*x/TILE_SIZE) < WIN_WIDTH/TILE_SIZE) && sd->cube->map[(int)(*y / TILE_SIZE)][(int)(*x / TILE_SIZE)] != '1')
+            sd->line->h_y = (abs(sd->p->y / 30) + 1) * 30;
+             sd->line->h_x = sd->p->x + ((sd->line->h_y - sd->p->y) / tan(ang));
+            while (((int)(sd->line->h_y/TILE_SIZE) > 0 && (int)(sd->line->h_y/TILE_SIZE) < WIN_HEIGHT/TILE_SIZE) && ((int)(sd->line->h_x/TILE_SIZE) > 0 && (int)(sd->line->h_x/TILE_SIZE) < WIN_WIDTH/TILE_SIZE) && sd->cube->map[(int)(sd->line->h_y / TILE_SIZE)][(int)(sd->line->h_x / TILE_SIZE)] != '1')
             {
-                *y += TILE_SIZE;
-                *x += (TILE_SIZE / tan(ang));
+                sd->line->h_y += TILE_SIZE;
+                sd->line->h_x += (TILE_SIZE / tan(ang));
             }
-            // ft_draw_line(sd, ang, *x, *y);
+            // ft_draw_line(sd, ang, sd->line->h_x, sd->line->h_y);
         }
     }
-    if ((ang <  ((3 *M_PI)/2) && ang >= M_PI) ||(ang < M_PI &&  ang > (M_PI/2)))
+    if (sd->line->ray_up == 1 && sd->line->ray_left == 1)
     {
-
         if (ang <  ((3 *M_PI)/2) && ang >= M_PI)
         {
-            *y = abs(sd->p->y / 30) * 30;
-            *x = sd->p->x - ((sd->p->y - *y) / tan(ang));
-            while (((int)(*y/TILE_SIZE) > 0 && (int)(*y/TILE_SIZE) < WIN_HEIGHT/TILE_SIZE) && ((int)(*x/TILE_SIZE) > 0 && (int)(*x/TILE_SIZE) < WIN_WIDTH/TILE_SIZE) &&sd->cube->map[(int)(*y / TILE_SIZE)][(int)(*x / TILE_SIZE)] != '1')
+            sd->line->h_y = abs(sd->p->y / 30) * 30;
+            sd->line->h_x = sd->p->x - ((sd->p->y - sd->line->h_y) / tan(ang));
+            while (((int)(sd->line->h_y/TILE_SIZE) > 0 && (int)(sd->line->h_y/TILE_SIZE) < WIN_HEIGHT/TILE_SIZE) && ((int)(sd->line->h_x/TILE_SIZE) > 0 && (int)(sd->line->h_x/TILE_SIZE) < WIN_WIDTH/TILE_SIZE) &&sd->cube->map[(int)(sd->line->h_y / TILE_SIZE)][(int)(sd->line->h_x / TILE_SIZE)] != '1')
             {
-                *y -= TILE_SIZE;
-                *x -= (TILE_SIZE / tan(ang));
+                sd->line->h_y -= TILE_SIZE;
+                sd->line->h_x -= (TILE_SIZE / tan(ang));
             }
         }
-        if (ang < M_PI &&  ang > (M_PI/2))
+        if (sd->line->ray_down == 1 && sd->line->ray_left == 1)
         {
-            *y = (abs(sd->p->y / 30) + 1) * 30;
-            *x = sd->p->x - ((*y - sd->p->y) / tan(ang));
+            // puts("2\n");
+            sd->line->h_y = (abs(sd->p->y / 30) + 1) * 30;
+            sd->line->h_x = sd->p->x - ((sd->line->h_y - sd->p->y) / tan(ang));
             if (ang >  ((3 *M_PI)/2) && ang < (2 *M_PI))
             {
                 
-                while ((sd->cube->map[(int)(*y / TILE_SIZE)][(int)(*x / TILE_SIZE)] != '1'))
+                while ((sd->cube->map[(int)(sd->line->h_y / TILE_SIZE)][(int)(sd->line->h_x / TILE_SIZE)] != '1'))
                  {
-                    *y += TILE_SIZE;
-                    *x -= (TILE_SIZE / tan(ang));
+                    sd->line->h_y += TILE_SIZE;
+                    sd->line->h_x -= (TILE_SIZE / tan(ang));
                 }
             }
         }
--- display/vertical_check.c
+++ display/vertical_check.c
@@ -6,38 +6,38 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 11:34:32 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/01/25 11:26:25 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/02/04 11:13:21 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-void ft_vertical_check(t_data    *sd, double ang, double  *y, double  *x)
+void ft_vertical_check(t_data    *sd, double ang)
 {
-    if ((ang >  ((3 *M_PI)/2) && ang < (2 *M_PI)) ||(ang >= 0 &&  ang < (M_PI/2)))
+    if (sd->line->ray_right == 1)
     {
-        *(x) = (abs(sd->p->x / 30) + 1) * 30;
-        *(y) = sd->p->y - (sin(ang) * ((*(x) - sd->p->x) / cos(ang)));
-        if (ang >  ((3 *M_PI)/2) && ang < (2 *M_PI))
+        sd->line->v_x = (abs(sd->p->x / 30) + 1) * 30;
+       sd->line->v_y = sd->p->y - (sin(ang) * ((sd->line->v_x - sd->p->x) / cos(ang)));
+        if (sd->line->ray_up == 1)
         {
             
-            while (((int)(*y/TILE_SIZE) > 0 && (int)(*y/TILE_SIZE) < WIN_HEIGHT/TILE_SIZE) && ((int)(*x/TILE_SIZE) > 0 && (int)(*x/TILE_SIZE) < WIN_WIDTH/TILE_SIZE) &&(sd->cube->map[(int)(*y / TILE_SIZE)][(int)(*x / TILE_SIZE)] != '1'))
+            while (((int)(sd->line->v_y/TILE_SIZE) > 0 && (int)(sd->line->v_y/TILE_SIZE) < WIN_HEIGHT/TILE_SIZE) && ((int)(sd->line->v_x/TILE_SIZE) > 0 && (int)(sd->line->v_x/TILE_SIZE) < WIN_WIDTH/TILE_SIZE) &&(sd->cube->map[(int)(sd->line->v_y / TILE_SIZE)][(int)(sd->line->v_x / TILE_SIZE)] != '1'))
              {
-                *(x) += TILE_SIZE;
-                *(y) = *y - (sin(ang)* (TILE_SIZE/cos(ang)));
+                sd->line->v_x += TILE_SIZE;
+               sd->line->v_y = sd->line->v_y - (sin(ang)* (TILE_SIZE/cos(ang)));
             }
-            // ft_draw_line(sd, ang, *x, *y);
+            // ft_draw_line(sd, ang, sd->line->v_x, sd->line->v_y);
         }
-       if (( ang < (2 *M_PI)) ||(ang >= 0 &&  ang < (M_PI/2)))
+       if (sd->line->ray_down == 1)
         {
-                *x = (abs(sd->p->x / 30) + 1) * 30;
-                *y = sd->p->y + (sin(ang)* ((sd->p->x - *x) / cos(ang)));
-                while (((int)(*y/TILE_SIZE) > 0 && (int)(*y/TILE_SIZE) < WIN_HEIGHT/TILE_SIZE) && ((int)(*x/TILE_SIZE) > 0 && (int)(*x/TILE_SIZE) < WIN_WIDTH/TILE_SIZE) &&(sd->cube->map[(int)(*y / TILE_SIZE)][(int)(*x / TILE_SIZE)] != '1' ))
+                sd->line->v_x = (abs(sd->p->x / 30) + 1) * 30;
+                sd->line->v_y = sd->p->y + (sin(ang)* ((sd->p->x - sd->line->v_x) / cos(ang)));
+                while (((int)(sd->line->v_y/TILE_SIZE) > 0 && (int)(sd->line->v_y/TILE_SIZE) < WIN_HEIGHT/TILE_SIZE) && ((int)(sd->line->v_x/TILE_SIZE) > 0 && (int)(sd->line->v_x/TILE_SIZE) < WIN_WIDTH/TILE_SIZE) &&(sd->cube->map[(int)(sd->line->v_y / TILE_SIZE)][(int)(sd->line->v_x / TILE_SIZE)] != '1' ))
                  {
-                    *x += TILE_SIZE;
-                    *y = *y + sin(ang)* (TILE_SIZE/cos(ang));
+                    sd->line->v_x += TILE_SIZE;
+                    sd->line->v_y = sd->line->v_y + sin(ang)* (TILE_SIZE/cos(ang));
                 }
-                // ft_draw_line(sd, ang, *x, *y);
+                // ft_draw_line(sd, ang, sd->line->v_x, *y);
         }
         // ft_draw_line(sd, ang, x, y);
     }
